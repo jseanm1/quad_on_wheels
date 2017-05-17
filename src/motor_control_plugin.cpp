33d4adc8e5e0001a04602d1bb6ee8bc7d64bcf4c
Janindu <janindu1@gmail.com>
2017-05-17 12:47:00 +1000

    Ros subacriber stub added to the motor controller plugin. Implemented #define guards for the header file. Commented out the applied motor forces in onUpdate

--- src/motor_control_plugin.cpp
+++ src/motor_control_plugin.cpp
@@ -8,7 +8,19 @@ namespace gazebo {
   }
 
   void MotorControlPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr) {
+    if (!ros::isInitialized()) {
+      std::cout << "ROS is not initialized" << std::endl;
+
+      int argc = 0;
+      char **argv = NULL;
+      ros::init(argc, argv, "motor_control_plugin");
+    }
+    std::cout << "ROS is initialized" << std::endl;
     std::cout << "Loading MotorControlPlugin..." << std::endl;
+
+    // Setup subscribers
+    ros::NodeHandle nh;
+    this->simpleControllerSub = nh.subscribe("/controller/simple", 10, &MotorControlPlugin::simpleControllerCb, this);
 
     this->model = parent;
 
@@ -23,12 +35,16 @@ namespace gazebo {
   }
 
   void MotorControlPlugin::onUpdate(const common::UpdateInfo&) {
-    gazebo::math::Vector3 force(0.0, 0.0, 10.0);
+    // gazebo::math::Vector3 force(0.0, 0.0, 10.0);
 
-    linkM1->AddRelativeForce(force);
-    linkM2->AddRelativeForce(force);
-    linkM3->AddRelativeForce(force);
-    linkM4->AddRelativeForce(force);
+    // linkM1->AddRelativeForce(force);
+    // linkM2->AddRelativeForce(force);
+    // linkM3->AddRelativeForce(force);
+    // linkM4->AddRelativeForce(force);
+  }
+
+  void MotorControlPlugin::simpleControllerCb(geometry_msgs::Wrench::ConstPtr wrench) {
+    std::cout << "Msg recieved" << std::endl;
   }
 
   GZ_REGISTER_MODEL_PLUGIN(MotorControlPlugin)
